9017327bee68e02c9fc807e289833625c34cb993
SihanCai <cai_s3@denison.edu>
2019-06-08 04:33:43 +0800

    Add files via upload
    Add Header background

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" @touchmove.prevent>
+    <v-header :seller="seller"></v-header>
+    <div class="tab-wrapper">
+      <tab :tabs="tabs"></tab>
+    </div>
+  </div>
+</template>
+
+<script>
+  import VHeader from 'components/v-header/v-header'
+
+  export default {
+    components: {
+      VHeader
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+</style>
